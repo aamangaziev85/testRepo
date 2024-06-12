01bf394c0b3574f1cb7ceea01263e09bd9942c94
Azat <aamangaziev85@gmail.com>
2024-06-12 17:09:20 +0600

    new commit

---
+++ src/main/java/com/example/demo5/Car.java
@@ -0,0 +1,23 @@
+package com.example.demo5;
+
+public class Car {
+    private String name;
+    private String name2;
+    private int year;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
